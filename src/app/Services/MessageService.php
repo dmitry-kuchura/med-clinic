37979211a8328019cf6569ac60677dc195b5224f
Dmytro Kuchura <kuchura.d@gmail.com>
2021-04-03 23:48:12 +0300

    changes

--- src/app/Services/MessageService.php
+++ src/app/Services/MessageService.php
@@ -2,40 +2,68 @@
 
 namespace App\Services;
 
-use App\Facades\MessageFacade;
-use App\Facades\MessageTemplatesFacade;
 use App\Helpers\TurboSMS;
 use App\Models\PatientAppointment;
+use App\Repositories\MessagesRepository;
+use App\Repositories\MessagesTemplatesRepository;
+use App\Repositories\PatientsMessagesRepository;
 
 class MessageService
 {
+    const RECORDS_AT_PAGE = 30;
+
     /** @var TurboSMS */
     private TurboSMS $smsSender;
 
-    /** @var MessageFacade */
-    private MessageFacade $messageFacade;
+    /** @var MessagesRepository */
+    private MessagesRepository $messageRepository;
 
-    /** @var MessageTemplatesFacade */
-    private MessageTemplatesFacade $messageTemplatesFacade;
+    /** @var PatientsMessagesRepository */
+    private PatientsMessagesRepository $patientsMessagesRepository;
+
+    /** @var MessagesTemplatesRepository */
+    private MessagesTemplatesRepository $messageTemplatesRepository;
 
     public function __construct(
-        MessageFacade $messageFacade,
-        MessageTemplatesFacade $messageTemplatesFacade
+        MessagesRepository $messageRepository,
+        PatientsMessagesRepository $patientsMessagesRepository,
+        MessagesTemplatesRepository $messageTemplatesRepository
     )
     {
         $this->smsSender = new TurboSMS();
-        $this->messageFacade = $messageFacade;
-        $this->messageTemplatesFacade = $messageTemplatesFacade;
+        $this->messageRepository = $messageRepository;
+        $this->patientsMessagesRepository = $patientsMessagesRepository;
+        $this->messageTemplatesRepository = $messageTemplatesRepository;
     }
 
     public function getPatientMessageList(int $id)
     {
-        return $this->messageFacade->paginate($id);
+        return $this->patientsMessagesRepository->paginate($id, self::RECORDS_AT_PAGE);
     }
 
     public function send(array $request, array $response)
     {
-        $this->messageFacade->send($request, $response);
+        $result = $response['response_result'];
+
+        foreach ($result as $value) {
+            $messageData = [
+                'type' => 'sms',
+                'text' => $request['text'],
+                'recipient' => '+' . $value['phone'],
+                'message_id' => $value['message_id'],
+                'response_code' => $value['response_code'],
+                'response_status' => $value['response_status']
+            ];
+
+            $message = $this->messageRepository->store($messageData);
+
+            if ($request['patient_id']) {
+                $this->patientsMessagesRepository->store([
+                    'patient_id' => $request['patient_id'],
+                    'message_id' => $message->id
+                ]);
+            }
+        }
     }
 
     public function sendPatientMessage(array $request)
@@ -52,12 +80,12 @@ class MessageService
 
     public function listTemplates()
     {
-        return $this->messageTemplatesFacade->paginate();
+        return $this->messageTemplatesRepository->paginate(self::RECORDS_AT_PAGE);
     }
 
     public function getMessageTemplate(int $id)
     {
-        return $this->messageTemplatesFacade->find($id);
+        return $this->messageTemplatesRepository->get($id);
     }
 
     public function remindBeforeDay(PatientAppointment $patientAppointment)
