90a0277b5be39760faeee6fe128b1d70363b2218
Dmytro Kuchura <kuchura.d@gmail.com>
2021-05-24 11:08:42 +0300

    fix LAB доктор

---
+++ src/database/migrations/2021_05_24_104751_update_doctors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class UpdateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('doctors')
+            ->where('first_name', 'like','%LAB%')
+            ->where('last_name', 'like','%LAB%')
+            ->where('middle_name', 'like','%LAB%')
+            ->update(['is_lab' => true]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
